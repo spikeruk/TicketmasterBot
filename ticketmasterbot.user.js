d590e0efa08321ee012eb95e27f07f67fdb0f3de
spikeruk <30730481+spikeruk@users.noreply.github.com>
2017-08-05 09:05:28 +0100

    Update ticketmasterbot.user.js

--- ticketmasterbot.user.js
+++ ticketmasterbot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TicketMaster
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Fast execution of reserving tickets in cart
 // @match        https://www1.ticketmaster.co.uk/*
 // @match        https://www1.ticketmaster.com/*
@@ -34,6 +34,27 @@ function ProcessFilterPanel(filterBar){
     
     //Change ticket quantity (if applicable)
     waitForElement('.offer-card', function() {
+        
+        //Change the number of tickets (if applicable);
+        ChangeTicketQuantity();
+        
+        //Click the button to Buy the tickets (right hand panel)
+        var getTicketsElement = ClickElement('//button[@id = "offer-card-buy-button"]'); 
+
+        //Sometimes a dialog comes up if someone else beat us to the tickets.
+        //This dialog gives a recommendation for a new seat selection.
+        //If this occurs, we choose to accept the new seats.
+        waitForElement('.button-aux, .modal-dialog__button', function() {
+          var sectionChangeBuyButton = getElementByXpath('//button[@class = "button-aux modal-dialog__button"]');
+          sectionChangeBuyButton.click();
+        });
+        
+
+    });
+}
+
+function ChangeTicketQuantity()
+{
         var rightPanelCurrentTicketCountElement = getElementByXpath('//div[@class = "qty-picker__number qty-picker__number--lg"]');
         var currentTicketCount = rightPanelCurrentTicketCountElement.innerText;
 
@@ -55,20 +76,18 @@ function ProcessFilterPanel(filterBar){
                 try{ticketDecrementElement.click();}catch(ex){}
             }
         }
-        
-        //Click the button to Buy the tickets (right hand panel)
-        var getTicketsElement = ClickElement('//button[@id = "offer-card-buy-button"]'); 
+}
 
-        //Sometimes a dialog comes up if someone else beat us to the tickets.
-        //This dialog gives a recommendation for a new seat selection.
-        //If this occurs, we choose to accept the new seats.
-        waitForElement('.button-aux, .modal-dialog__button', function() {
-          var sectionChangeBuyButton = getElementByXpath('//button[@class = "button-aux modal-dialog__button"]');
-          sectionChangeBuyButton.click();
-        });
-        
-
-    });       
+function CheckForGeneralAdmission()
+{
+    var BuyButton = getElementByXpath('//button[@id = "offer-card-buy-button"]');
+    return BuyButton;
+}
+    
+function ProcessGeneralAdmission(generalAdmissionBuyButton)
+{
+    ChangeTicketQuantity();
+    generalAdmissionBuyButton.click();  
 }
 
 function reload() {
@@ -76,45 +95,25 @@ function reload() {
 }
 
 
-$(document).ready(function() {
-    
-    //This popup dialog seems to happen in the US ticketmaster website
-    //We just close it down and continue as normal
-    SkipPopup();
-    
-    //Ticket type 1
-    //This occurs in the majority of ticket sales when there is a selection of ticket types
-    var filterBar = CheckForFilterPanel();
-    if(filterBar)
-    {
-        ProcessFilterPanel(filterBar);
-    }
-    //TODO: Add more 'else if' options (i.e. Some ticket sales don't have a filter bar, In this scenario it just a case of setting the ticket quantity and pressing the buy button
-    else
-    {
-        //refresh the page after an interval (Tickets weren't yet on sale)
-        setTimeout(function(){reload();}, refreshIntervalSeconds * 1000); 
-    }
-});
-
-
-
-function ClickElement(path, time) {
+function ClickElement(path, time) 
+{
     var element = getElementByXpath(path);
-        if(element !== null) {
-            if (typeof element.click != 'undefined')
-            {
-                element.click();
-                return element;
-            }
+    if(element !== null) {
+        if (typeof element.click != 'undefined')
+        {
+            element.click();
+            return element;
         }
+    }
 }
 
-function getElementByXpath(path) {
+function getElementByXpath(path) 
+{
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-var waitForElement = function(selector, callback) {
+var waitForElement = function(selector, callback) 
+{
   if (jQuery(selector).length) {
     callback();
   } else {
@@ -123,4 +122,49 @@ var waitForElement = function(selector, callback) {
     }, 100);
   }
 };
+
+$(document).ready(function() 
+{   
+    var success=false;
+    //This popup dialog seems to happen in the US ticketmaster website
+    //We just close it down and continue as normal
+    SkipPopup();
+    
+    //Ticket type 1
+    //This occurs in the majority of ticket sales when there is a selection of ticket types
+    if(!success)
+    {
+        var filterBar = CheckForFilterPanel();
+        if(filterBar)
+        {
+            console.log('These tickets have a filter bar');
+            success=true;
+            ProcessFilterPanel(filterBar);
+        }
+    }
+    
+    //Ticket type 2
+    //These tickets are General Admission and do not have assigned seating (i.e. no filter bar)
+    if(!success)
+    {
+        var generalAdmissionBuyButton = CheckForGeneralAdmission();
+        if(generalAdmissionBuyButton)
+        {
+            console.log('These tickets are General Admission');
+            success=true;
+            ProcessGeneralAdmission(generalAdmissionBuyButton);
+        }
+    }
+    
+    //TODO: Add more ticket types if found
+
+    if(!success)
+    {
+        //refresh the page after an interval (Tickets weren't yet on sale)
+        setTimeout(function(){reload();}, refreshIntervalSeconds * 1000); 
+    }
+});
+
+
+
 
